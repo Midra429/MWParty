b602799ef9b3f08b9409b479a08b67192fae8789
Midra <me@midra.me>
2025-02-02 01:07:57 +0900

    親ディレクトリでも動くように, README削除

--- packages/backend/patch_allowed_origins.sh
+++ packages/backend/patch_allowed_origins.sh
@@ -1,4 +1,6 @@
-source ./.env
+# https://clerk.com/docs/deployments/deploy-chrome-extension
+
+source "$(dirname "$0")/.env"
 
 curl -X PATCH https://api.clerk.com/v1/instance \
   -H "Authorization: Bearer $CLERK_SECRET_KEY" \
@@ -8,4 +10,5 @@ curl -X PATCH https://api.clerk.com/v1/instance \
       \"chrome-extension://$CHROME_EXTENSION_ID\",
       \"moz-extension://$FIREFOX_EXTENSION_ID\"
     ]
-  }"
+  }" \
+  -w "%{http_code}\n"
