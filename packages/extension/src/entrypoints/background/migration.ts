6e0e4594ce38b6452fb0e4d682ce2ea50ca72e9e
Midra <me@midra.me>
2025-02-03 16:28:39 +0900

    無駄にsetしない

--- packages/extension/src/entrypoints/background/migration.ts
+++ packages/extension/src/entrypoints/background/migration.ts
@@ -4,7 +4,8 @@ import { storage } from '@/utils/storage/extension'
 const STORAGE_VERSION = 1
 
 export default async () => {
-  let version = (await storage.get('_version')) ?? STORAGE_VERSION
+  let oldVersion = await storage.get('_version')
+  let newVersion = oldVersion ?? STORAGE_VERSION
 
   // // v0.0.0 -> v0.0.0
   // if (version < 2) {
@@ -13,5 +14,7 @@ export default async () => {
   //   version = 2
   // }
 
-  await storage.set('_version', version)
+  if (newVersion !== oldVersion) {
+    await storage.set('_version', newVersion)
+  }
 }
