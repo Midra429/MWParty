7d9eb819034f3bd87be679234eaac6e24aec9b9e
Midra <me@midra.me>
2025-02-04 15:21:50 +0900

    Firefoxを削除

--- packages/backend/patch_allowed_origins.sh
+++ packages/backend/patch_allowed_origins.sh
@@ -7,8 +7,7 @@ curl -X PATCH https://api.clerk.com/v1/instance \
   -H "Content-type: application/json" \
   -d "{
     \"allowed_origins\": [
-      \"chrome-extension://$CHROME_EXTENSION_ID\",
-      \"moz-extension://$FIREFOX_EXTENSION_ID\"
+      \"chrome-extension://$CHROME_EXTENSION_ID\"
     ]
   }" \
   -w "%{http_code}\n"
